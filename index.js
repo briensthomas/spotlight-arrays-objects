9a3b10ea492a375c6819952b90ebc09e70326713
Brien Thomas <thomasbrien94@gmail.com>
2022-06-23 14:08:05 -0700

    getSum function sets an acc value at 0, and adds each number from the array to the accumulator

--- index.js
+++ index.js
@@ -39,7 +39,14 @@ export const printUserInfo = ({
 // REQS: use rest parameters
 //  getSum(1, 2, 3) === 6
 //  getSum(1, 2, 3, 4, 5) === 15
-export const getSum = () => {}
+export const getSum = (...rest) => {
+  let acc = 0;
+
+  for (let num of rest) {
+    acc = acc + num
+  }
+  return acc;
+}
 
 // INPUT: an unknown number of arguments
 // OUTPUT: an array with the first two arguments destructured and the remaining in a nested array
